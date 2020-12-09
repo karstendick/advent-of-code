bd021d032bf83bee84ab766a08d32d1819104a37
Joshua Karstendick <joshdick@gmail.com>
2020-12-09 18:43:05 -0500

    solved day 9, part 2

--- 2020/day9/day9_part2.py
+++ 2020/day9/day9_part2.py
@@ -1,26 +1,16 @@
-from itertools import chain, combinations
-
-def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
-    s = list(iterable)
-    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
-
 lines = []
-with open('example_input.txt', 'r') as f:
+with open('input.txt', 'r') as f:
     for line in f:
       line = line.strip()
       lines.append(int(line))
 
-window = 5 # 25
-for i, num in enumerate(lines):
-    if i < window:
-        continue
-    found_sum = False
-    for xi, x in enumerate(lines[i-window:i]):
-        for yi, y in enumerate(lines[i-window:i]):
-            if xi == yi:
-                continue
-            if x + y == lines[i]:
-                found_sum = True
-    if not found_sum:
-        print(lines[i])
+# target = 127
+target = 258585477
+for i in range(len(lines)):
+    for j in range(i, len(lines)):
+        subarray = lines[i:j+1]
+        s = sum(subarray)
+        if s == target:
+            mmin = min(subarray)
+            mmax = max(subarray)
+            print(mmin + mmax)
