4f8d41c7a889693b99da4825d476b2ee744ae7a7
Joshua Karstendick <joshdick@gmail.com>
2021-12-24 21:52:40 -0500

    solved day 7

---
+++ 2021/day7/day7_part2.py
@@ -0,0 +1,25 @@
+with open('input.txt', 'r') as f:
+  lines = [l.strip() for l in f.readlines()]
+
+crabs = [int(x) for x in lines[0].split(',')]
+min_crab = min(crabs)
+max_crab = max(crabs)
+# print(f'{crabs}, {min_crab}, {max_crab}')
+
+def triag(n):
+  return (n * (n+1))//2
+
+def get_fuel(crabs, pos):
+  fuel = 0
+  for crab in crabs:
+    fuel += triag(abs(crab - pos))
+  return fuel
+
+min_fuel = None
+for pos in range(min_crab, max_crab+1):
+  fuel = get_fuel(crabs, pos)
+  # print(f'pos, fuel: {pos}, {fuel}')
+  if not min_fuel or fuel < min_fuel:
+    min_fuel = fuel
+
+print(min_fuel)
