1987bb4af700b56ebea2271c5d38001c30e916ec
Joshua Karstendick <joshdick@gmail.com>
2023-03-05 21:01:01 -0500

    solved day 9, part 1, 2022

---
+++ 2022/day9/day9_part2.py
@@ -0,0 +1,79 @@
+inp = []
+with open('example.txt', 'r') as f:
+  for line in f:
+    inp.append(line.strip())
+
+motions = []
+for line in inp:
+  dir, n = line.split()
+  motions.append([dir, int(n)])
+
+# print(motions)
+
+hx, hy = 0, 0
+tx, ty = 0, 0
+
+def move_head(dir, n, hx, hy):
+  if dir == 'R':
+    hx += n
+  elif dir == 'L':
+    hx -= n
+  elif dir == 'U':
+    hy += n
+  elif dir == 'D':
+    hy -= n
+  else:
+    raise Exception(f"received invalid direction {dir}")
+  return hx, hy
+
+def sgn(x):
+  if x > 0:
+    return 1
+  elif x < 0:
+    return -1
+  else:
+    return 0
+
+def move_tail(hx, hy, tx, ty):
+  dx = hx - tx
+  dy = hy - ty
+  if abs(dx) <= 1 and abs(dy) <= 1:
+    pass
+  elif abs(dx) == 2 and abs(dy) == 0:
+    if dx == 2:
+      tx += 1
+    elif dx == -2:
+      tx -= 1
+  elif abs(dx) == 0 and abs(dy) == 2:
+    if dy == 2:
+      ty += 1
+    elif dy == -2:
+      ty -= 1
+  else:
+    # print(f"Moving tail diagonally: ({sgn(dx)},{sgn(dy)})")
+    tx += sgn(dx)
+    ty += sgn(dy)
+  return tx, ty
+  
+tail_positions = set()
+tail_positions.add((tx, ty))
+for motion in motions:
+  dir, n = motion
+  for i in range(n):
+    hx, hy = move_head(dir, 1, hx, hy)
+    tx, ty = move_tail(hx, hy, tx, ty)
+    tail_positions.add((tx, ty))
+
+  # print(f"{motion} to H: ({hx},{hy}) T: ({tx},{ty})")
+
+# print(tail_positions)
+print(len(tail_positions))
+
+
+
+
+
+
+
+
+
