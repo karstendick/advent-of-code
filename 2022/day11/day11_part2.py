be327832e83c9ade8cb9418bed9d13b6b005ccc3
Joshua Karstendick <joshdick@gmail.com>
2023-11-23 15:47:08 -0500

    solved day11, part 2!

--- 2022/day11/day11_part2.py
+++ 2022/day11/day11_part2.py
@@ -1,58 +1,120 @@
+from operator import add, mul
+
+class Monkey:
+  def __init__(self, number, items, operator, leftop, rightop, divisor, true_target, false_target):
+    self.number = number
+    self.items = items
+    self.operator = operator
+    self.divisor = divisor
+    self.leftop = leftop
+    self.rightop = rightop
+    self.true_target = true_target
+    self.false_target = false_target
+    self.num_inspections = 0
+  def __repr__(self):
+    return f'Monkey {self.number} with items {self.items} and operator {self.operator} and divisor {self.divisor} and leftop {self.leftop} and rightop {self.rightop} and true_target {self.true_target} and false_target {self.false_target}'
+
+
 inp = []
 with open('input.txt', 'r') as f:
   for line in f:
     inp.append(line.strip())
 
-instructions = []
+monkeys = []
+
+num = 0
+items = []
+operator = None
+divisor = None
+leftop = None
+rightop = None
+true_target = None
+false_target = None
 for line in inp:
-  instruction = line.split()
-  if len(instruction) == 2:
-    instruction[1] = int(instruction[1])
-  instructions.append(instruction)
+  if line.startswith('Monkey'):
+    num = int(line.split(' ')[1].rstrip(':'))
+  elif line.startswith('Starting items'):
+    items = [int(x) for x in line.split(':')[1].split(',')]
+  elif line.startswith('Operation'):
+    line = line.split(':')[1].strip()
+    _,_,leftop, operator, rightop = line.split(' ')
+  elif line.startswith('Test'):
+    divisor = int(line.split(' ')[-1])
+  elif line.startswith('If true'):
+    true_target = int(line.split(' ')[-1])
+  elif line.startswith('If false'):
+    false_target = int(line.split(' ')[-1])
+    monkeys.append(Monkey(num, items, operator, leftop, rightop, divisor, true_target, false_target))
 
-# print(instructions)
+# print(monkeys)
 
-X = 1
-cycle = 0
-cycle_to_X = {}
-cycle_to_X[0] = 1
 
-for inst in instructions:
-  # print(f'During cycle {cycle}, X is {X}')
-  if inst[0] == 'noop':
-    cycle += 1
-    cycle_to_X[cycle] = X
-  elif inst[0] == 'addx':
-    operand = inst[1]
-    
-    cycle += 1
-    cycle_to_X[cycle] = X
-    cycle += 1
-    cycle_to_X[cycle] = X
-    
-    X += operand
+# calculate product of a list of numbers
+def prod(numbers):
+  p = 1
+  for n in numbers:
+    p *= n
+  return p
+
+divisor_product = prod([monkey.divisor for monkey in monkeys])
+
+def calc_operation(item, monkey):
+  if monkey.operator == '*':
+    operator = mul
+  elif monkey.operator == '+':
+    operator = add
   else:
-    raise Exception("Unexpected instruction!")
-  
-  # cycle_to_X.append(X)
-# print(f'After cycle {cycle}, X is {X}')
-cycle_to_X[cycle+1] = X
-# print(cycle_to_X)
-
-crt = ['.' for i in range(245)]
-for cycle in range(1,241):
-  X = cycle_to_X[cycle]
-  pixel_pos = (cycle-1)
-  if (pixel_pos % 40) in [X-1, X, X+1]:
-    # print(f'In cycle {cycle}, drawing pixel # in position {cycle-1} with X={X} & sprite=[{X-1}, {X}, {X+1}]')
-    crt[pixel_pos] = '#'
+    raise Exception(f'Unknown operator {monkey.operator}')
+  if monkey.leftop == 'old':
+    left = item
   else:
-    # print(f'In cycle {cycle}, drawing pixel . in position {cycle-1} with X={X} & sprite=[{X-1}, {X}, {X+1}]')
-    pass
+    left = int(monkey.leftop)
+  if monkey.rightop == 'old':
+    right = item
+  else:
+    right = int(monkey.rightop)
+  return operator(left, right)
 
-print(''.join(crt[0:40]))
-print(''.join(crt[40:80]))
-print(''.join(crt[80:120]))
-print(''.join(crt[120:160]))
-print(''.join(crt[160:200]))
-print(''.join(crt[200:240]))
+def take_one_turn(monkeys, i):
+  monkey = monkeys[i]
+  while len(monkey.items) != 0:
+    monkey.num_inspections += 1
+    item = monkey.items.pop(0)
+    item = calc_operation(item, monkey)
+    # item = item // 3 # worry no longer decreases!
+    item = item % divisor_product # we're in Z_{divisor_product}!
+
+    if item % monkey.divisor == 0:
+      monkeys[monkey.true_target].items.append(item)
+    else:
+      monkeys[monkey.false_target].items.append(item)
+  return monkeys
+
+def one_round(monkeys):
+  for i in range(len(monkeys)):
+    monkeys = take_one_turn(monkeys, i)
+  return monkeys
+
+
+
+# take 10000 rounds
+for i in range(10000):
+  if i % 100 == 0:
+    print(f'Round {i}')
+  monkeys = one_round(monkeys)
+
+# caulcate the top 2 integers from a list
+def top_two(items):
+  top1 = 0
+  top2 = 0
+  for item in items:
+    if item > top1:
+      top2 = top1
+      top1 = item
+    elif item > top2:
+      top2 = item
+  return top1, top2
+
+top1, top2 = top_two([monkeys[i].num_inspections for i in range(len(monkeys))])
+monkey_biz = top1 * top2
+print(f'Monkey biz is {monkey_biz}')
