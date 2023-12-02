32d04ef818b4cb1a6533b634915e0978459e7577
Joshua Karstendick <joshdick@gmail.com>
2023-12-02 10:24:20 -0500

    solved day 1, part 2

--- 2023/day1/day1_part2.py
+++ 2023/day1/day1_part2.py
@@ -1,58 +1,41 @@
-import networkx as nx
-
+import re
 inp = []
 with open('input.txt', 'r') as f:
   for line in f:
     inp.append(line.strip())
 
-starts = []
-end = ''
-elevation_map = []
-for row, s in enumerate(inp):
-  elevation_row = []
-  for col, ch in enumerate(s):
-    if ch == 'S':
-      elevation = 0
-      starts.append(f'r{row}c{col}')
-    elif ch == 'E':
-      elevation = 26
-      end = f'r{row}c{col}'
-    else:
-      elevation = ord(ch) - ord('a')
-      if elevation == 0:
-        starts.append(f'r{row}c{col}')
-    elevation_row.append(elevation)
-  elevation_map.append(elevation_row)
 
-# print(elevation_map)
-# for row in elevation_map:
-#   print(row)
+digits_regex = re.compile(r'\d|one|two|three|four|five|six|seven|eight|nine')
 
-def can_reach(start_row, start_col, end_row, end_col, elevation_map):
-  start_elevation = elevation_map[start_row][start_col]
-  end_elevation = elevation_map[end_row][end_col]
-  if start_elevation >= end_elevation - 1:
-    return True
-  else:
-    return False
+def str_to_int(string):
+  if string == 'one':
+    return 1
+  if string == 'two':
+    return 2
+  if string == 'three':
+    return 3
+  if string == 'four':
+    return 4
+  if string == 'five':
+    return 5
+  if string == 'six':
+    return 6
+  if string == 'seven':
+    return 7
+  if string == 'eight':
+    return 8
+  if string == 'nine':
+    return 9
+  return int(string)
+
+sum = 0
+for line in inp:
+  first_digit = re.findall(digits_regex, line)[0]
+  last_digit = re.findall(digits_regex, line)[-1]
+  num = 10*str_to_int(first_digit) + str_to_int(last_digit)
+  sum += num
 
 
 
-edges = []
-for row in range(len(elevation_map)):
-  for col in range(len(elevation_map[0])):
-    if (row > 0) and can_reach(row, col, row-1, col, elevation_map):
-      edges.append((f'r{row}c{col}', f'r{row-1}c{col}', 1))
-    if (row < len(elevation_map) - 1) and can_reach(row, col, row+1, col, elevation_map):
-      edges.append((f'r{row}c{col}', f'r{row+1}c{col}', 1))
-    if (col > 0) and can_reach(row, col, row, col-1, elevation_map):
-      edges.append((f'r{row}c{col}', f'r{row}c{col-1}', 1))
-    if (col < len(elevation_map[0]) - 1) and can_reach(row, col, row, col+1, elevation_map):
-      edges.append((f'r{row}c{col}', f'r{row}c{col+1}', 1))
+print(sum)
   
-# print(edges)
-
-G = nx.DiGraph()
-G.add_weighted_edges_from(edges)
-
-print(nx.multi_source_dijkstra(G, starts, end)[0])
