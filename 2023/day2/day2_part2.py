ceea49befdadae68acb5e695c2ee9bc4f931e0c0
Joshua Karstendick <joshdick@gmail.com>
2023-12-02 21:18:55 -0500

    solved day 2, part 2

--- 2023/day2/day2_part2.py
+++ 2023/day2/day2_part2.py
@@ -3,32 +3,25 @@ with open('input.txt', 'r') as f:
   for line in f:
     inp.append(line.strip())
 
-# print(inp)
 
-# only 12 red cubes, 13 green cubes, and 14 blue cubes
-maxes = {'red': 12, 'green': 13, 'blue': 14}
-
-possible_games_sum = 0
+power_sum = 0
 game_num = 1
 for line in inp:
   game_valid = True
   game_str = line.split(':')[1].strip()
   grabs = game_str.split(';')
+  current_maxes = {'red': 0, 'green': 0, 'blue': 0}
   for grab in grabs:
     subsets = grab.split(',')
     for subset in subsets:
       num, color = subset.strip().split(' ')
       color = color.strip()
       num = int(num.strip())
-      if num > maxes[color]:
-        # print(f'Game {game_num} has too many {color} cubes')
-        game_valid = False
-        break
-  if game_valid:
-    possible_games_sum += game_num
+      if num > current_maxes[color]:
+        current_maxes[color] = num
+  power = current_maxes['red'] * current_maxes['green'] * current_maxes['blue']
+  power_sum += power
   game_num += 1
 
 
-
-print(possible_games_sum)
-  
+print(power_sum)
