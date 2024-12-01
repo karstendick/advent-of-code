39e4491c97c2b4a0fe4e012194111f9298fc8ad6
Joshua Karstendick <joshdick@gmail.com>
2024-12-01 09:53:07 -0500

    solved day1, part2

---
+++ 2024/day01/day1_part2.py
@@ -0,0 +1,29 @@
+from collections import Counter
+
+inp = []
+with open('input.txt', 'r') as f:
+  for line in f:
+    inp.append([int(num) for num in line.split()])
+
+print(inp)
+
+def transpose(matrix):
+  return list(map(list, zip(*matrix)))
+
+m = transpose(inp)
+print(m)
+# m = [sorted(l) for l in m]
+# print(m)
+left, right = m
+print(left, right)
+
+right_freq = Counter(right)
+print(right_freq)
+
+scores = []
+for e in left:
+  score = e * right_freq[e]
+  scores.append(score)
+print(scores)
+total = sum(scores)
+print(total)
