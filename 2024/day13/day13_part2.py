6955e75f1ea15e35fc3290c43cd52004b53f4b6f
Joshua Karstendick <joshdick@gmail.com>
2024-12-15 21:54:11 -0500

    solved day 13, part 2

--- 2024/day13/day13_part2.py
+++ 2024/day13/day13_part2.py
@@ -1,10 +1,13 @@
 from collections import namedtuple
 from itertools import product
+from sympy import symbols, Eq, solve
 
 Machine = namedtuple('Machine', ['ax', 'ay', 'bx', 'by', 'px', 'py'])
 
+ten_trillion = 10_000_000_000_000
+
 machines = []
-with open('example.txt', 'r') as f:
+with open('input.txt', 'r') as f:
   for line in f:
     line = line.strip()
     segments = line.replace('+', ' ').replace(',', ' ').replace('=', ' ').split()
@@ -15,8 +18,8 @@ with open('example.txt', 'r') as f:
       bx = int(segments[3])
       by = int(segments[5])
     elif line.startswith('Prize:'):
-      px = int(segments[2])
-      py = int(segments[4])
+      px = int(segments[2]) + ten_trillion
+      py = int(segments[4]) + ten_trillion
       machine = Machine(ax, ay, bx, by, px, py)
       machines.append(machine)
 
@@ -24,15 +27,13 @@ with open('example.txt', 'r') as f:
 
 total_tokens = 0
 for machine in machines:
-  cand_tokens = []
-  button_presses = list(product(range(101), range(101)))
-  for (apresses, bpresses) in button_presses:
-    clawx = machine.ax * apresses + machine.bx * bpresses
-    clawy = machine.ay * apresses + machine.by * bpresses
-    if clawx == machine.px and clawy == machine.py:
-      tokens = 3 * apresses + 1 * bpresses
-      cand_tokens.append(tokens)
-  if cand_tokens:
-    total_tokens += min(cand_tokens)
+  a, b = symbols('a b', integer=True)
+  eq1 = Eq(machine.ax * a + machine.bx * b, machine.px)
+  eq2 = Eq(machine.ay * a + machine.by * b, machine.py)
+  soln = solve((eq1, eq2), (a, b))
+  if soln:
+    apresses, bpresses = soln[a], soln[b]
+    tokens = 3 * apresses + 1 * bpresses
+    total_tokens += tokens
 
 print(total_tokens)
